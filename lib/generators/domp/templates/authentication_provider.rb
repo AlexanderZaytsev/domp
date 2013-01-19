00c529af4499476722bc0bfa454ca77784e3536e
Alexander Zaytsev <alexander@say26.com>
2013-01-20 00:17:45 +0500

    Add has_many :model_authentications

--- lib/generators/domp/templates/authentication_provider.rb
+++ lib/generators/domp/templates/authentication_provider.rb
@@ -1,6 +1,7 @@
 <% module_namespacing do -%>
 class AuthenticationProvider < ActiveRecord::Base
   has_many :<%= table_name %>
+  has_many :<%= class_name.downcase %>_authentications
 <% if Rails::VERSION::MAJOR < 4 -%>
   attr_accessible :name
 <% end -%>
